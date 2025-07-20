14de193dae221aa33ebd036844244ca0a829c65d
Abubakar Kassim <142273851+RSG0@users.noreply.github.com>
2025-07-20 13:15:16 -0500

    added pile_1 player information

--- red_blue_nim.py
+++ red_blue_nim.py
@@ -16,14 +16,78 @@ def assign_rocks(num_red, num_blue):
 
     return pile_1, pile_2
 
-def standard():
+def has_duplicates(data, color):
+    new_list = []
+    for d in data:
+        # print(d)
+        if d == color:
+            new_list.append(d)
+    print("New List:", new_list)
+    print("Counts:", len(new_list) )
+    # print("Counts:", len(counts.values()), "\n")
+    # print(any(count >= 2 for count in counts.values()))
+    if (len(new_list) > 1):
+        return True
+    else: 
+        return False
+    
+    # return any(count >= 2 for count in counts.values())
+
+def remove_two_marbles(pile, color):
+    scan = print("You've picked 2", color ,"marbles")
+    if (has_duplicates(pile, color)):
+        pile.remove(color)
+        pile.remove(color)
+        print("Pile now has:", len(pile), "rocks left")
+    else:
+        print("Not enough blue marbles")
+
+def remove_one_marble(pile, color):
+    scan = print("You've picked 1 red marble")
+    if (color in pile):
+        pile.remove(color)
+    else:
+        print("Not enough", color, "marbles")
+
+def standard(pile_1, pile_2):
     print("This will be Standard")
-    scan = input("Enter q to exit\n")
 
     while True:
-        scan = input("How about now\n")
+        print("Player 1: Your Turn")
+        print("1. Choose Pile 1")
+        print("2. Choose Pile 2")
+        scan = input("Enter q to exit\n")
+
         if scan == 'q':
             break
+        if scan == '1':
+            print("You've chosen Pile 1:\n")
+            player_choice()
+            scan = input("\n")
+
+            if scan == '1':
+                remove_two_marbles(pile_1, 'red')
+            elif scan == '2':
+                remove_two_marbles(pile_1, 'blue')
+            elif scan == '3':
+                remove_one_marble(pile_1, 'red')
+            elif scan == '4':
+                remove_one_marble(pile_1, 'blue')
+            elif scan == 'q':
+                break
+            
+        elif scan == '2':
+            print("You've chosen Pile 2:")
+        else:
+            scan = print("Invalid choice. Please choose 1 or 2\n")
+
+def player_choice():
+    print("Select your choice")
+    print("1. Pick 2 red marbles")
+    print("2. Pick 2 blue marbles")
+    print("3. Pick 1 red marble")
+    print("4. Pick 2 red marble")
+
 def misere():
     print("This will be Misere")
     scan = input("Enter q to exit\n")
@@ -60,7 +124,7 @@ def main():
     scan = input()
     if scan == "1":
         print("You are playing Classic Red Blue Nim.")
-        standard()
+        standard(pile_1=pile_1, pile_2=pile_2)
     elif scan == "2":
         print("You are playing Misere Red Blue Nim.")
         misere()
