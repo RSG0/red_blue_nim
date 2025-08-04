8f8c1ec6835a8fdc13074e770db79744550f6fd9
Abubakar Kassim <142273851+RSG0@users.noreply.github.com>
2025-08-04 17:09:33 -0500

    Add misere mode
    need to test

--- red_blue_nim.py
+++ red_blue_nim.py
@@ -60,16 +60,23 @@ def try_move(state, amount, color):
     except:
         return None
 
-def generate_moves(state):
+def generate_moves(state, misere=False): # assume misere is false by default
     moves = []
     pile = state.pile
-
-    move_order = [
-        ('2', 'red'),   # 2 red
-        ('2', 'blue'),  # 2 blue
-        ('1', 'red'),   # 1 red
-        ('1', 'blue')   # 1 blue
-    ]
+    if misere:
+        move_order = [ # order should get inveretd if misere is True
+            ('1', 'blue'),
+            ('1', 'red'),
+            ('2', 'blue'),
+            ('2', 'red')
+        ]
+    else:
+        move_order = [
+            ('2', 'red'),
+            ('2', 'blue'),
+            ('1', 'red'),
+            ('1', 'blue')
+        ]
 
     for amount, color in move_order:
         new_state = try_move(state, amount, color)
@@ -93,19 +100,19 @@ def assign_rocks(num_red, num_blue):
 
     return pile_1, pile_2
 
-def player_choosing_marbles(pile, choice, pile_name):
+def player_choosing_marbles(pile, choice, pile_name, misere=False):
     if choice == '1':
         if (remove_two_marbles(pile, 'red', player_marbles) != -1):
-            computer_turn(pile, pile_name)
+            computer_turn(pile, pile_name, misere)
     elif choice == '2':
         if (remove_two_marbles(pile, 'blue', player_marbles) != -1):
-            computer_turn(pile, pile_name)
+            computer_turn(pile, pile_name, misere)
     elif choice == '3':
         if (remove_one_marble(pile, 'red', player_marbles) != -1 ):
-            computer_turn(pile, pile_name)
+            computer_turn(pile, pile_name, misere)
     elif choice == '4':
         if (remove_one_marble(pile, 'blue', player_marbles) != -1):
-            computer_turn(pile, pile_name)
+            computer_turn(pile, pile_name, misere)
     elif choice == 'q':
         return 'quit'
     else:
@@ -121,19 +128,27 @@ def calculate_standard_score(marbles, score):
             # print("blue is in")
             score -= 1
     return score
+def calculate_misere_score(marbles, score):
+    for marble in marbles:
+        if marble == 'red':
+            # print("red is in")
+            score += 2
+        elif marble == 'blue':
+            # print("blue is in")
+            score += 1
+    return score
 
-def computer_turn(pile_contents, pile_name):
+def computer_turn(pile_contents, pile_name, misere=False):
     print("COM is thinking...")
-    # time.sleep(2); #will be removed in final version
     state = GameState(pile_contents, player_marbles, computer_marbles, True)
-    _, best_move = minimax(state, depth=4, alpha=-float('inf'), beta=float('inf'))
+    _, best_move = minimax(state, depth=4, alpha=-float('inf'), beta=float('inf'), misere=misere)
 
     if best_move is None:
         print("COM cannot make a move.")
         return
 
     amount, color = best_move
-    print(f"COM chooses to pick {amount} {color} marble(s) from {pile_name}" ) #HERE
+    print(f"COM chooses to pick {amount} {color} marble(s) from {pile_name}" )
 
     if amount == '2':
         remove_two_marbles(pile_contents, color, computer_marbles)
@@ -141,16 +156,17 @@ def computer_turn(pile_contents, pile_name):
         remove_one_marble(pile_contents, color, computer_marbles)
     print("COM has chosen")
 
+
 #REMINDER: beta should be pos infinity , and alpha should be neg infinity for worst possible options
-def minimax(state, depth, alpha, beta):
+def minimax(state, depth, alpha, beta, misere=False):
     if depth == 0 or state.is_terminal():
         return state.get_score(), None
 
     if state.is_max_turn:
         max_eval = -float('inf')
         best_move = None
-        for amount, color, new_state in generate_moves(state):
-            eval_score, _ = minimax(new_state, depth - 1, alpha, beta)
+        for amount, color, new_state in generate_moves(state, misere=misere):
+            eval_score, _ = minimax(new_state, depth - 1, alpha, beta, misere=misere)
             if eval_score > max_eval:
                 max_eval = eval_score
                 best_move = (amount, color)
@@ -161,8 +177,8 @@ def minimax(state, depth, alpha, beta):
     else:
         min_eval = float('inf')
         best_move = None
-        for amount, color, new_state in generate_moves(state):
-            eval_score, _ = minimax(new_state, depth - 1, alpha, beta)
+        for amount, color, new_state in generate_moves(state, misere=misere):
+            eval_score, _ = minimax(new_state, depth - 1, alpha, beta, misere=misere)
             if eval_score < min_eval:
                 min_eval = eval_score
                 best_move = (amount, color)
@@ -170,6 +186,7 @@ def minimax(state, depth, alpha, beta):
             if beta <= alpha:
                 break
         return min_eval, best_move
+
 
 def has_duplicates(data, color):
     new_list = []
@@ -261,14 +278,47 @@ def display_player_choice():
     print("3. Pick 1 red marble")
     print("4. Pick 1 blue marble")
 
-def misere():
+def misere(pile_1, pile_2):
     print("This will be Misere")
-    scan = input("Enter q to exit\n")
-
+    global player_score
     while True:
-        scan = input("How about now\n")
+
+        if (len(pile_1) == 0 or len(pile_2) == 0):
+            player_score = calculate_misere_score(player_marbles, player_score)
+            print("Pile is empty");
+            print("You lose!")
+            print("Player score is", player_score )
+            break
+
+        print("Player 1: Your Turn")
+        print("1. Choose Pile 1")
+        print("2. Choose Pile 2")
+        print("3. Look at Pile")
+        print("4. Number of Points")
+        scan = input("Enter q to exit\n")
+
+
         if scan == 'q':
             break
+        if scan == '1': # choose pile 1
+            print("You've chosen Pile 1:")
+            display_player_choice()
+            choice = input("\n")
+            if player_choosing_marbles(pile_1, choice, "Pile 1", True) == 'quit':
+                break
+        elif scan == '2': # choose pile 2
+            print("You've chosen Pile 2:\n")
+            display_player_choice()
+            choice = input("\n")
+            if player_choosing_marbles(pile_2, choice, "Pile 2", True) == 'quit':
+                break
+        elif scan == '3': # display pile
+            print("Pile 1: ", pile_1)
+            print("Pile 2: ", pile_2)
+        elif scan == '4': # display score
+            print("Player Marbles:\n",player_marbles)
+        else:
+            scan = print("Invalid choice. Please choose 1 or 2\n")
 
 def main():
     if len(sys.argv) != 3:
@@ -277,6 +327,7 @@ def main():
     try:
         num_red = int(sys.argv[1])      #string that represents command land argument
         num_blue = int(sys.argv[2])     #string that represents command land argument
+        # need to add misere
     except:
         print("Error: num-red and num-blue must be integers.")
         sys.exit(1)
@@ -301,6 +352,6 @@ def main():
         standard(pile_1, pile_2)
     elif scan == "2":
         print("You are playing Misere Red Blue Nim.")
-        misere()
+        misere(pile_1, pile_2)
 if __name__ == "__main__":
     main()
